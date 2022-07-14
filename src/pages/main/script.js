0aa8b103179e669f86358fb7951adbf901d5c388
Oleg Skarednov <olegskar1@gmail.com>
2022-07-14 22:19:01 +0500

    feat burger menu

--- src/pages/main/script.js
+++ src/pages/main/script.js
@@ -1,1 +1,21 @@
 // Burger menu
+const burger = document.querySelector('.burger');
+const headerNav = document.querySelector('.header__nav');
+let logo;
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  headerNav.classList.toggle('active');
+
+  if (burger.classList.contains('active')) {
+    logo = document.createElement('div');
+    logo.className = 'header__logo';
+    logo.innerHTML = `<a href="#!">
+    <h1>Cozy House</h1>
+    <p>Shelter for pets in Boston</p>
+  </a>`;
+    headerNav.prepend(logo);
+  } else {
+    logo.remove();
+  }
+});
