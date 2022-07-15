2c266e4120b3089ff6b3d9eb6f7d3500d1a86d33
Oleg Skarednov <olegskar1@gmail.com>
2022-07-15 21:01:51 +0500

    feat main burger-menu

--- src/pages/main/script.js
+++ src/pages/main/script.js
@@ -1,8 +1,43 @@
 // Burger menu
 const burger = document.querySelector(".burger");
 const headerNav = document.querySelector(".header__nav");
+const body = document.body;
+const navLinks = headerNav.querySelectorAll(".header__nav>a");
 
-burger.addEventListener("click", () => {
+const activeLink = () => {
+  navLinks.forEach((link) => {
+    if (link.classList.contains("active")) {
+      link.classList.remove("active");
+    }
+  });
+};
+
+headerNav.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("header__link")) {
+    return;
+  } else {
+    if (!e.target.classList.contains("active")) {
+      activeLink();
+      e.target.classList.add("active");
+      burgerToggle();
+    } else {
+      burgerToggle();
+    }
+  }
+});
+
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", (e) => {
+//     if (e.target.classList.contains("active")) {
+//       return;
+//     }
+//     e.target.classList.add("active");
+//   });
+// });
+const burgerToggle = () => {
   burger.classList.toggle("active");
   headerNav.classList.toggle("active");
-});
+  body.classList.toggle("lock");
+};
+
+burger.addEventListener("click", burgerToggle);
