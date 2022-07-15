aecbe0d3a2827e24839815c30730f342a19b9a98
Oleg Skarednov <olegskar1@gmail.com>
2022-07-15 18:13:07 +0500

    feat interactive burger button

--- src/pages/main/script.js
+++ src/pages/main/script.js
@@ -1,21 +1,8 @@
 // Burger menu
-const burger = document.querySelector('.burger');
-const headerNav = document.querySelector('.header__nav');
-let logo;
+const burger = document.querySelector(".burger");
+const headerNav = document.querySelector(".header__nav");
 
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  headerNav.classList.toggle('active');
-
-  if (burger.classList.contains('active')) {
-    logo = document.createElement('div');
-    logo.className = 'header__logo';
-    logo.innerHTML = `<a href="#!">
-    <h1>Cozy House</h1>
-    <p>Shelter for pets in Boston</p>
-  </a>`;
-    headerNav.prepend(logo);
-  } else {
-    logo.remove();
-  }
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  headerNav.classList.toggle("active");
 });
