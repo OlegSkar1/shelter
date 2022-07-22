dbb73999626edcd1829dec05272f371a82ea70f7
Oleg Skarednov <olegskar1@gmail.com>
2022-07-22 20:44:17 +0500

    feat desktop slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,44 +1,45 @@
-import petsData from "./pets.js";
-import PetCard from "./PetCard.js";
+import petsData from './pets.js';
+import PetCard from './PetCard.js';
 
-const BTN_LEFT = document.querySelector("#btn-left");
-const BTN_RIGHT = document.querySelector("#btn-right");
-const CAROUSEL = document.querySelector("#pets-carousel");
-const ITEM_ACTIVE = document.querySelector("#container-active");
-const ITEM_LEFT = document.querySelector("#container-left");
-const ITEM_RIGHT = document.querySelector("#container-right");
+const BTN_LEFT = document.querySelector('#btn-left');
+const BTN_RIGHT = document.querySelector('#btn-right');
+const CAROUSEL = document.querySelector('#pets-carousel');
+const ITEM_ACTIVE = document.querySelector('#container-active');
+const ITEM_LEFT = document.querySelector('#container-left');
+const ITEM_RIGHT = document.querySelector('#container-right');
 
 // перелистывание влево
 const moveLeft = () => {
-  CAROUSEL.classList.add("transition-left");
-  BTN_LEFT.removeEventListener("click", moveLeft); // запрещаем перелистывать пока не закончится анимация
-  BTN_RIGHT.removeEventListener("click", moveRight);
+  CAROUSEL.classList.add('transition-left');
+  BTN_LEFT.removeEventListener('click', moveLeft); // запрещаем перелистывать пока не закончится анимация
+  BTN_RIGHT.removeEventListener('click', moveRight);
 };
 
 // перелистывание вправо
 const moveRight = () => {
-  CAROUSEL.classList.add("transition-right");
-  BTN_RIGHT.removeEventListener("click", moveRight);
-  BTN_LEFT.removeEventListener("click", moveLeft);
+  CAROUSEL.classList.add('transition-right');
+  BTN_RIGHT.removeEventListener('click', moveRight);
+  BTN_LEFT.removeEventListener('click', moveLeft);
 };
 
-BTN_RIGHT.addEventListener("click", moveRight);
-BTN_LEFT.addEventListener("click", moveLeft);
+BTN_RIGHT.addEventListener('click', moveRight);
+BTN_LEFT.addEventListener('click', moveLeft);
 
-CAROUSEL.addEventListener("animationend", (animation) => {
-  let changedItems;
-  if (animation.animationName === "move-left") {
-    changedItems = ITEM_LEFT;
-    CAROUSEL.classList.remove("transition-left");
+CAROUSEL.addEventListener('animationend', (animation) => {
+  if (animation.animationName === 'move-left') {
+    CAROUSEL.classList.remove('transition-left');
+    ITEM_RIGHT.innerHTML = ITEM_ACTIVE.innerHTML; // переписываем элементы центра на правую позицию
     ITEM_ACTIVE.innerHTML = ITEM_LEFT.innerHTML; // переписываем элементы слева на центральную позицию
+    generateLeftContainer(petsData);
   } else {
-    CAROUSEL.classList.remove("transition-right");
-    changedItems = ITEM_RIGHT;
+    CAROUSEL.classList.remove('transition-right');
+    ITEM_LEFT.innerHTML = ITEM_ACTIVE.innerHTML;
     ITEM_ACTIVE.innerHTML = ITEM_RIGHT.innerHTML;
+    generateRightContainer(petsData);
   }
 
-  BTN_RIGHT.addEventListener("click", moveRight);
-  BTN_LEFT.addEventListener("click", moveLeft);
+  BTN_RIGHT.addEventListener('click', moveRight);
+  BTN_LEFT.addEventListener('click', moveLeft);
 });
 
 // создаем ноды из массива объектов питомцев
@@ -50,7 +51,7 @@ const createPetCards = (data) => {
   return cards;
 };
 
-// генерируем массив из 3х случайных уникальных индексов
+// генерируем массив из случайных уникальных индексов
 const generateRandomArray = (data) => {
   let setCard = new Set();
   while (setCard.size < 3) {
@@ -60,9 +61,9 @@ const generateRandomArray = (data) => {
   return [...setCard];
 };
 
-// Рендерим те карточки, data-id которых совпадают с setCard
+// Рендерим те карточки, data-id которых совпадают с generateRandomArray
 const renderFiltredCards = (data, item) => {
-  item.innerHTML = "";
+  item.innerHTML = '';
   let randomArr = generateRandomArray(data);
 
   // создаем ноды из массива объектов питомцев
@@ -75,6 +76,21 @@ const renderFiltredCards = (data, item) => {
   });
 };
 
+// генерируем для right/left container уникальные data-id, отличные от active container
+const uniqCardId = (active, current, data) => {
+  let resultCardId = [...active, ...current];
+  let uniqCardId = new Set(resultCardId);
+
+  //если были удалены повторяющиеся числа, то наполняем Set
+  while (uniqCardId.size < resultCardId.length) {
+    let randomId = Math.floor(Math.random() * data.length) + 1; // случайный Id от 1 до max включая границы
+    uniqCardId.add(randomId);
+  }
+
+  // получаем уникальный массив
+  return [...uniqCardId].slice(3);
+};
+
 // генерируем ITEM_ACTIVE из случайных карточек
 const generateActiveContainer = (data) => {
   renderFiltredCards(data, ITEM_ACTIVE);
@@ -84,45 +100,56 @@ const generateActiveContainer = (data) => {
 const generateLeftContainer = (data) => {
   renderFiltredCards(data, ITEM_LEFT); // создаем ITEM_LEFT
 
-  const activeCards = document.querySelectorAll("#container-active .pet-card");
-  const leftCards = document.querySelectorAll("#container-left .pet-card");
+  const activeCards = document.querySelectorAll('#container-active .pet-card');
+  const leftCards = document.querySelectorAll('#container-left .pet-card');
 
   // получаем массивы data-id
   let activeCardId = Array.from(activeCards).map((card) => +card.dataset.id);
   let leftCardId = Array.from(leftCards).map((card) => +card.dataset.id);
 
   // получаем результирующий массив и оставляем уникальные data-id
-  let resultCardId = [...activeCardId, ...leftCardId];
-  let uniqCardId = new Set(resultCardId);
-  console.log(resultCardId);
-  console.log(uniqCardId);
 
-  //если были удалены повторяющиеся числа, то наполняем Set до 6
-  while (uniqCardId.size < 6) {
-    let randomId = Math.floor(Math.random() * data.length) + 1; // случайный Id от 1 до max включая границы
-    uniqCardId.add(randomId);
-  }
-  console.log([...uniqCardId]);
-
-  // получаем уникальный массив для leftCards
-  const uniqLeftCardId = [...uniqCardId].slice(3);
-  console.log(uniqLeftCardId);
+  let uniqCard = uniqCardId(activeCardId, leftCardId, data);
 
   //рендерим leftContainer с уникальными карточками
-  ITEM_LEFT.innerHTML = "";
+  ITEM_LEFT.innerHTML = '';
   const petCards = createPetCards(data);
 
-  let filtredCards = petCards.filter((card) =>
-    uniqLeftCardId.includes(card.id)
-  );
+  let filtredCards = petCards.filter((card) => uniqCard.includes(card.id));
 
   filtredCards.forEach((card) => {
     ITEM_LEFT.append(card.generateCard());
   });
 };
 
+//генерируем ITEM_RIGHT из случайных карточек
+const generateRightContainer = (data) => {
+  renderFiltredCards(data, ITEM_RIGHT); // создаем ITEM_RIGHT
+
+  const activeCards = document.querySelectorAll('#container-active .pet-card');
+  const rightCards = document.querySelectorAll('#container-right .pet-card');
+
+  // получаем массивы data-id
+  let activeCardId = Array.from(activeCards).map((card) => +card.dataset.id);
+  let rightCardId = Array.from(rightCards).map((card) => +card.dataset.id);
+
+  // получаем результирующий массив и оставляем уникальные data-id
+
+  let uniqCard = uniqCardId(activeCardId, rightCardId, data);
+
+  //рендерим leftContainer с уникальными карточками
+  ITEM_RIGHT.innerHTML = '';
+  const petCards = createPetCards(data);
+
+  let filtredCards = petCards.filter((card) => uniqCard.includes(card.id));
+
+  filtredCards.forEach((card) => {
+    ITEM_RIGHT.append(card.generateCard());
+  });
+};
+
 window.onload = function () {
-  // ITEM_RIGHT.innerHTML = '';
   generateActiveContainer(petsData);
   generateLeftContainer(petsData);
+  generateRightContainer(petsData);
 };
