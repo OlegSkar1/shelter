d88880e8faabbb983bc6639112b81fa5459b1247
Oleg Skarednov <olegskar1@gmail.com>
2022-07-29 19:51:02 +0500

    fix buttons paginator inactive

--- src/pages/pets/pets.js
+++ src/pages/pets/pets.js
@@ -3,9 +3,11 @@ import '../../js/pagination.js';
 import petsData from '../../js/pets.js';
 import PetCard from '../../js/PetCard.js';
 import SliderModal from '../../js/SliderModal.js';
-import {createPetArray, numOfButtons, numOfElemsPerPage} from '../../js/pagination.js';
-
-
+import {
+  createPetArray,
+  numOfButtons,
+  numOfElemsPerPage,
+} from '../../js/pagination.js';
 
 const CONTENT = document.querySelector('.pets__content');
 const NAVIGATION = document.querySelector('.navigation');
@@ -16,12 +18,12 @@ const BUTTON_NEXT = document.querySelector('#button_next');
 const BUTTON_LAST = document.querySelector('#button_last');
 
 const createPetCards = (data) => {
-    let cards = [];
-    data.forEach((item) => {
-      cards.push(new PetCard(item));
-    });
-    return cards;
-  };
+  let cards = [];
+  data.forEach((item) => {
+    cards.push(new PetCard(item));
+  });
+  return cards;
+};
 
 const getResultPetArr = (data) => {
   let cards = createPetCards(data); // создаем массив карточек из petsData
@@ -29,81 +31,103 @@ const getResultPetArr = (data) => {
   let resultArr = [];
   for (let i = 0; i < page.length; i++) {
     resultArr.push(...page[i]); // разворачиваем массивы карточек в результирующий массив
-} 
-return resultArr;
-}
+  }
+  return resultArr;
+};
 const resultArr = getResultPetArr(petsData); // сохранили полученные данные в переменную
-
-
-const renderContent = (data) => {
-      const result = data.map(elem => CONTENT.append(elem.generateCard())); // рендерим карточки на страницу
-      addModalClickHandler(); 
-      return result;
-}
 
 //modal
 
 const addModalClickHandler = () => {
-    CONTENT.addEventListener('click', (e) => {
-      let card = e.target.closest('.pet-card');
-      if (card) {
-        let clickedCardId = card.getAttribute('data-id');
-        let clickedCardData = getClickedData(clickedCardId);
-  
-        renderModalWindow(clickedCardData);
-      }
-    });
-  };
-  
-  const renderModalWindow = (card) => {
-    let modal = new SliderModal('modal', card);
-    modal.renderModal();
-  };
-  
-  const getClickedData = (id) => {
-    return petsData.find((card) => card.id == id);
-  };
+  CONTENT.addEventListener('click', (e) => {
+    let card = e.target.closest('.pet-card');
+    if (card) {
+      let clickedCardId = card.getAttribute('data-id');
+      let clickedCardData = getClickedData(clickedCardId);
+      renderModalWindow(clickedCardData);
+    }
+  });
+};
 
-  // обработка событий при клике на кнопку пагинации
+const getClickedData = (id) => {
+  return petsData.find((card) => card.id == id);
+};
 
-  NAVIGATION.addEventListener('click', (e) => {
-    let numOfAllPages = numOfButtons(resultArr, numOfElemsPerPage);
-    let button = e.target;
-    let currPage = +BUTTON_ACTIVE.textContent;
-      if (button.closest('#button_next') && currPage < numOfAllPages) {
-        currPage += 1;
-        getPage(currPage);
-      } 
-      if (button.closest('#button_last') && currPage < numOfAllPages) {
-        getPage(numOfAllPages);
-      }
-      if (button.closest('#button_prev') && currPage > 1) {
-        currPage -= 1;
-        getPage(currPage);
-      }
-      if (button.closest('#button_first') && currPage > 1) {
-        currPage = 1;
-        getPage(currPage);
-      }
-      // if (button.closest('.button__paginator') && !button.closest('#button_active')) {
-      //   console.log('first')
-      // } здесь закончить с переключением стилей кнопок
-  })
+const renderModalWindow = (card) => {
+  let modal = new SliderModal('modal', card);
+  modal.renderModal();
+};
 
-  const getPage = (page) => {
-    BUTTON_ACTIVE.innerHTML = `<span>${page}</span>`;
-    let start = numOfElemsPerPage * (page - 1);
-    let end = numOfElemsPerPage * page;
-    CONTENT.innerHTML = '';
-    renderCurrPage(start, end);
+// обработка событий при клике на кнопку пагинации
+
+const renderContent = (data) => {
+  let result = data.map((elem) => CONTENT.append(elem.generateCard())); // рендерим карточки на страницу
+  // return result;
+};
+
+const renderCurrPage = (start, end) => {
+  let slicedArr = resultArr.slice(start, end);
+  renderContent(slicedArr);
+  return slicedArr;
+};
+
+const renderPagination = (e) => {
+  let numOfAllPages = numOfButtons(resultArr, numOfElemsPerPage);
+  let button = e.target;
+  let currPage = +BUTTON_ACTIVE.textContent;
+
+  if (button.closest('#button_next') && currPage < numOfAllPages) {
+    currPage += 1;
+    getPage(currPage, numOfAllPages);
   }
-
-  const renderCurrPage = (start, end) => {
-    let slicedArr = resultArr.slice(start, end);
-    renderContent(slicedArr);
+  if (button.closest('#button_last') && currPage < numOfAllPages) {
+    currPage = numOfAllPages;
+    getPage(currPage, numOfAllPages);
   }
-
-  window.onload = function() {
-    renderCurrPage(0, numOfElemsPerPage);
-    console.log(resultArr)
+  if (button.closest('#button_prev') && currPage > 1) {
+    currPage -= 1;
+    getPage(currPage, numOfAllPages);
   }
+  if (button.closest('#button_first') && currPage > 1) {
+    currPage = 1;
+    getPage(currPage, numOfAllPages);
+  }
+};
+
+const getPage = (page, numOfAllPages) => {
+  BUTTON_ACTIVE.innerHTML = `<span>${page}</span>`;
+  let start = numOfElemsPerPage * (page - 1);
+  let end = numOfElemsPerPage * page;
+  CONTENT.innerHTML = '';
+
+  changeButtonsClass(page, numOfAllPages);
+  renderCurrPage(start, end);
+};
+
+const changeButtonsClass = (currPage, numOfAllPages) => {
+  if (currPage > 1 && currPage < numOfAllPages) {
+    BUTTON_FIRST.classList.remove('button_inactive');
+    BUTTON_PREV.classList.remove('button_inactive');
+    BUTTON_LAST.classList.remove('button_inactive');
+    BUTTON_NEXT.classList.remove('button_inactive');
+  }
+  if (currPage == numOfAllPages) {
+    BUTTON_FIRST.classList.remove('button_inactive');
+    BUTTON_PREV.classList.remove('button_inactive');
+    BUTTON_LAST.classList.add('button_inactive');
+    BUTTON_NEXT.classList.add('button_inactive');
+  }
+  if (currPage == 1) {
+    BUTTON_FIRST.classList.add('button_inactive');
+    BUTTON_PREV.classList.add('button_inactive');
+    BUTTON_LAST.classList.remove('button_inactive');
+    BUTTON_NEXT.classList.remove('button_inactive');
+  }
+};
+
+NAVIGATION.addEventListener('click', renderPagination);
+
+window.onload = function () {
+  renderCurrPage(0, numOfElemsPerPage);
+  addModalClickHandler();
+};
